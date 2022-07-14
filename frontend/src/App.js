9a9907f6e3b7925b775abd1b0e74ec41450c8813
axe-rigged <alex20101@student.hamk.fi>
2022-07-14 18:04:50 +0000

    installed tailwind to work on project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="bg-slate-100">
+      <h1 className="text-green-700 mx-auto text-center">Hello JazzWikia</h1>
+      <p className="text-blue-600 text-xl mx-auto text-center">Nice</p>
     </div>
   );
 }
